02930f0d413e9a68bb5df03b3badcb1ad3ec22a9
Maria <mmorrison1978@gmail.com>
2016-06-25 10:23:17 -0600

    updated

--- main.js
+++ main.js
@@ -4,14 +4,7 @@ function getTimeRemaining(endtime) {
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
   var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  // var firefoxDays = 1469145600000 - Date.now()
 
-  // console.log(Date.now());
-  // console.log(new Date());
-  // console.log('*******');
-  // console.log(days);
-  // console.log(t);
-  // console.log(firefoxDays);
   return {
     'total': t,
     'days': days,
@@ -28,8 +21,6 @@ function initializeClock(id, endtime) {
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
   var test = clock.querySelector('.test')
-  // var firefoxDays = 1469145600000 - Date.now()
-
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
@@ -38,7 +29,6 @@ function initializeClock(id, endtime) {
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-    // test.innerHTML = firefoxDays
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
